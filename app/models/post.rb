9d6831d08c56e7a794ecf2311b7bb41989d72645
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-11-17 13:07:55 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
-  has_many :users, through: :comments
+  has_many :users, -> { distinct }, through: :comments
   accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes['name'].blank? }
 
 
