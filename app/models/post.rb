c7323596444f2b24accc1ca5ea2171a283331245
gracenak <gracenak@gmail.com>
2020-11-27 15:05:20 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  # accepts_nested_attributes_for :comments
 
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute| 
@@ -18,12 +17,12 @@ class Post < ActiveRecord::Base
   end
 
 
-  def comments_attributes=(comment_attributes)
-    comment_attributes.values.each do |comment_attribute| 
-      if comment_attribute[:content].present?
-      comment = Comment.find_or_create_by(comment_attribute)
-      self.comments << comment
-      end
-    end
-  end
-end
+#   def comments_attributes=(comment_attributes)
+#     comment_attributes.values.each do |comment_attribute| 
+#       if comment_attribute[:content].present?
+#       comment = Comment.find_or_create_by(comment_attribute)
+#       self.comments << comment
+#       end
+#     end
+#   end
+# end
