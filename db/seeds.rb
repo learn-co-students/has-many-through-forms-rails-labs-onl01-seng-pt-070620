5a3bc0ae157ed2758b501dbfd6c49f724c717030
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2020-11-30 16:54:45 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+    Post.create(title: "Post#{i + 1}", content: "Post #{i + 1}'s content")
+end
