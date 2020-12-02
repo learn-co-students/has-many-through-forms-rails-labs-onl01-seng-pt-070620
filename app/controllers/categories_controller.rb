c7c4c5fc8f5c204744fdef6e57022b019f513799
gracenak <gracenak@gmail.com>
2020-12-01 23:01:44 -0700

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,5 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @post = Post.find(params[:id])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], comment_attributes: [])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
 
