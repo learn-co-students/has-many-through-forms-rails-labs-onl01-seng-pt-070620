76ef40b36dd9c0a65777cf2bee923a18923716d7
kinnari <kinnari1022@yahoo.com>
2020-12-01 23:54:25 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
+  def user_attributes(user_attribute)
+    user_attribute.values.each do |attribute| 
+      if attribute[:username].present?
+      user = User.find_or_create_by(attribute)
+      if !self.user = user
+        self.build_user(:username => user[:username])
+      end
+      end
+    end
+  end
+  
 end
